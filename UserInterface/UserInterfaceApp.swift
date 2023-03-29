ca41ae5a5af97bb1c3d59fdc5ebd47d9f604807d
Rio Stokes <rio.stokes@gtech.co.uk>
2023-03-29 13:29:53 +0100

    Initial Commit

---
+++ UserInterface/UserInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterfaceApp.swift
+//  UserInterface
+//
+//  Created by Rio Stokes on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UserInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
