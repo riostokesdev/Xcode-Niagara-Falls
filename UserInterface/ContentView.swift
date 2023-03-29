29eb1c0775bef2d5a66c428b78344fc6c471fbd9
Rio Stokes <rio.stokes@gtech.co.uk>
2023-03-29 15:55:13 +0100

    Project completed with Niagara Falls Review Card

--- UserInterface/ContentView.swift
+++ UserInterface/ContentView.swift
@@ -9,13 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.systemTeal)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0){
+                Image("Niagara-Falls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit )
+                    .cornerRadius(15.0)
+                    .padding()
+           
+            HStack {
+                Text("Niagara Falls")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                
+                Spacer()
+                
+                VStack{
+                    
+                    HStack{
+                        Image(systemName: "star.fill")
+                            
+                        
+                        Image(systemName: "star.fill")
+                        
+                        Image(systemName: "star.fill")
+                        
+                        Image(systemName: "star.fill")
+                        
+                        Image(systemName: "star.leadinghalf.fill")
+                    }
+                        .foregroundColor(.orange)
+                        .font(.caption)
+                   
+                    
+                    Text("(Reviews 361)")
+                }
+            }
+            
+            Text("Come visit the falls for an experience of a lifetime.")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+    
+            
         }
-        .padding()
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            )
+            .padding()
+        }
+        
+            
     }
 }
 
